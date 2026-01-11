2da29bd84d873852ddc131aacc1ffbf0cac17123
gwizzle651 <gwizzle651@proton.me>
2026-01-10 18:02:28 -0600

    complete the introduction

--- main.py
+++ main.py
@@ -1,1 +1,89 @@
+import keyboard
+import os
+import choices
+from time import sleep
+
+global choiceIndex = 0
+
+def clear():
+    '''
+    Clears the terminal, allowing it to be easily read
+    '''
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+def selectFromList(items: list[str]) -> str:
+    '''
+    Displays a numbered list of items and returns the user's selection.
+
+    :param items: A list of strings.
+    :type items: list[str]
+    :return selection: The user's selection.
+    :rtype: str
+    '''
+    for index, item in enumerate(items):
+        print(f"[{index + 1}] {item}")
+    
+    choiceIndex = getInput(len(items)) - 1
+    selection = items[choice]
+
+    return selection
+
+def getInput(options: int) -> int:
+    '''
+    :param options: The amount of options, with the maximum of 10. (Buttons 1-0 on keyboard.)
+    :return choice: Returns an integer of 1-0, based on keyboard input.
+    '''
+    while True:
+        choice = keyboard.read_key()
+        try:
+            if int(choice) == 0:
+                choice = 10
+            if int(choice) > options:
+                choice = options
+
+            return int(choice)
+        
+        except Exception:
+            choice = options
+
+            return int(choice)
+
+
+# game start
+
+# add possible call to start music.
+
+introduction()
+
+sleep(3)
+selection = selectFromList(dayStatus)
+print(f"You: \"{selection}\"\n")
+sleep(2)
+
+if choiceIndex != 0:
+   meetingInquiry()
+choiceIndex = 0
+
+print("You: \"Anyways, let us begin.\"\n")
+sleep(1)
+print("You: \"Tell me about...\"\n\n")
+
+selection = selectFromList(firstQuestion)
+print(f"You: \"{selection}\"\n")
+sleep(2)
+print("Gatsby: \"Yes, yes, let me think...\"\n")
+sleep(2)
+
+if choiceIndex == 0:
+    pass
+    #passion()
+elif choiceIndex == 1:
+    pass
+    #ambition()
+elif choiceIndex == 2:
+    pass
+    #greed()
 
---
+++ words.py
@@ -0,0 +1,37 @@
+from time import sleep
+
+# player choices
+dayStatus = ["It doesn't matter how my day has been.", "It goes as well as any day does for one such as myself."]
+firstQuestion = ["Your feelings towards Tom.", "How you got to where you are now in life.", "What you think of your peers."]
+
+# yapping
+def introduction():
+    print("It is the summer of 1922 in New York. Jay Gatsby, the mysterious, wealthy socialite of West Egg village is dying. He does not know it yet. But you must save him. \n\n--In a sitting room--\nGatsby sits accross from you on a plush sofa. He is shaken, his thoughts nervous and scattered. However, the joy emanating from him is undeniable.\n\n Gatsby: \"Tell me, old sport, how has the day treated you?\"\n\n")
+
+def meetingInquiry():
+    print("\"You: Tell me about your meeting, earlier today.\"\n")
+    sleep(2)
+    print("Gatsby: \"It was... eventful. It was the first time I've seen her in five years.\"\n")
+    sleep(2)
+    print("You: \"How did it go?\"\n")
+    sleep(1)
+    print("Gatsby: \"She still loves me, I know she does.\"\n")
+    sleep(2)
+    print("You: \"And she is still with Buchanan?\"\n")
+    sleep(2)
+    print("Gatsby: \"Yes, yes, of course...\"\n")
+    sleep(1)
+    print("You: \"...\"\n")
+
+def passion():
+    pass
+    #insert yap session here
+
+def ambition():
+    pass
+    #insert yap session here
+
+def greed():
+    pass
+    #insert yap session here
+
